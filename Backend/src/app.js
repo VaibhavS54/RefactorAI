e80fa4f15bd1c5a41da99be010fd9b93be1b5526
VaibhavS54 <vaibhav.2201190me@iiitbh.ac.in>
2025-08-24 13:10:11 +0530

    cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,7 +4,11 @@ const cors = require('cors')
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: "https://refactor-ai-rho.vercel.app",
+  methods: ["GET", "POST"],
+  credentials: true
+}));
 
 
 app.use(express.json())
